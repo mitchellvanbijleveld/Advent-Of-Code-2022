0b46357bb136f9e6ee9691195127aaa39d50b75c
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2022-12-29 18:06:49 +0100

    Update Day 5.py

--- Day 5.py
+++ Day 5.py
@@ -1,32 +1,31 @@
-#
-# [M] [H]         [N]                
-# [S] [W]         [F]     [W] [V]    
-# [J] [J]         [B]     [S] [B] [F]
-# [L] [F] [G]     [C]     [L] [N] [N]
-# [V] [Z] [D]     [P] [W] [G] [F] [Z]
-# [F] [D] [C] [S] [W] [M] [N] [H] [H]
-# [N] [N] [R] [B] [Z] [R] [T] [T] [M]
-# [R] [P] [W] [N] [M] [P] [R] [Q] [L]
+# [G]                 [D] [R]        
+# [W]         [V]     [C] [T] [M]    
+# [L]         [P] [Z] [Q] [F] [V]    
+# [J]         [S] [D] [J] [M] [T] [V]
+# [B]     [M] [H] [L] [Z] [J] [B] [S]
+# [R] [C] [T] [C] [T] [R] [D] [R] [D]
+# [T] [W] [Z] [T] [P] [B] [B] [H] [P]
+# [D] [S] [R] [D] [G] [F] [S] [L] [Q]
 #  1   2   3   4   5   6   7   8   9 
 
 countMoves = 0
 
-Stack1 = ["R","N","F","V","L","J","S","M"]
-Stack2 = ["P","N","D","Z","F","J","W","H"]
-Stack3 = ["W","R","C","D","G"]
-Stack4 = ["N","B","S"]
-Stack5 = ["M","Z","W","P","C","B","F","N"]
-Stack6 = ["P","R","M","W"]
-Stack7 = ["R","T","N","G","L","S","W"]
-Stack8 = ["Q","T","H","F","N","B","V"]
-Stack9 = ["L","M","H","Z","N","F"]
+Stack1 = ["D","T","R","B","J","L","W","G"]
+Stack2 = ["S","W","C"]
+Stack3 = ["R","Z","T","M"]
+Stack4 = ["D","T","C","H","S","P","V"]
+Stack5 = ["G","P","T","L","D","Z"]
+Stack6 = ["F","B","R","Z","J","Q","C","D"]
+Stack7 = ["S","B","D","J","M","F","T","R"]
+Stack8 = ["L","H","R","B","T","V","M"]
+Stack9 = ["Q","P","D","S","V"]
 
 STACKS = [Stack1,Stack2,Stack3,Stack4,Stack5,Stack6,Stack7,Stack8,Stack9]
 
 for stackie in STACKS:
     print(stackie)
 
-with open("Day-5.in") as file:
+with open("Day 5.input") as file:
     Moves = [i for i in file.read().strip().split("\n")]
 
     for Move in Moves:
